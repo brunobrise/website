550fe91e1281800a40a86dc19ce6026d63529cc2
Bruno Brito Semedo <ego@brunobrise.com>
2020-03-14 21:58:28 +0100

    add home page cover section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,26 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
+	#avatar {
+		height: 110vh;
 	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
+	.avatar-image {
 		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+		height: 100%;
+		background: url(cover.webp) no-repeat center center;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		background-size: cover;
+		position:relative;
 	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Bruno Brise</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<section id="avatar">
+	<div class="avatar-image"></div>
+</section>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
