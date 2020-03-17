bc03afc31c5a41e4248f870a4120e99bcc888e17
Bruno Brito Semedo <ego@brunobrise.com>
2020-03-17 15:12:52 +0100

    add career experience section

---
+++ src/routes/career/_career.js
@@ -0,0 +1,238 @@
+const career = {
+	experiences : [
+		{
+			organization: {
+				name: "ectoCo",
+				url: 'https://ectoco.com',
+				logo: "organization-ectoco.webp",
+				cover: 'organization-ectoco-cover.webp'
+			},
+			jobs :[
+				{
+					name: "Consultant DevOps",
+					contract: "Auto-entreprise",
+					date: "Depuis Fév 2015",
+					duration: "",
+					localisation: "Paris, France",
+					description: `
+						Architecture Cloud
+						Opérations et Développement (SRE)
+						Development Fullstack
+					`
+				}
+			],
+		},
+		{
+			organization: {
+				name: "Wynd",
+				url: 'https://wynd.eu',
+				logo: "organization-wynd.webp",
+				cover: 'organization-wynd-cover.webp'
+			},
+			jobs : [
+				{
+					name: "Ingénieur DevOps - SRE",
+					contract: "CDI",
+					date: "Sep 2018 - May 2020",
+					duration: "1 an 8 mois",
+					localisation: "Paris 15, France",
+					description: `
+						Migration de l'infrastructure existante vers le cloud :<br>
+						<ul>
+							<li>Architecture baséer sur la plateforme Azure</li>
+							<li>Installation et configuration de Kubernetes</li>
+							<li>Supervision SLA en accords avec les contrats client</li>
+							<li>Gestion des accès utilisateurs (RBAC)</li>
+							<li>Implémentation de la méthode Agile</li>
+							<li>Formation de l'équipe Admin. Sys. aux pratiques DevOps</li>
+						</ul>
+					`
+				},
+				{
+					name: "Consultant DevOps",
+					contract: "Indépendant",
+					date: "Sep 2017 – Sep 2018",
+					duration: "1 an",
+					localisation: "Paris 08, France",
+					description: `
+					Opérations sur l'infrastructure :
+					<ul>
+						<li>Augmentation de la haute disponibilité</li>
+						<li>Stabilisation du cluster base de données</li>
+						<li>Gestion des communications sécurisées (VPN)</li>
+					</ul>
+					Développement des services DevOps :
+					<ul>
+						<li>Pipeline CI/CD</li>
+						<li>Environnement éphémère</li>
+					</ul>
+					`
+				}
+			],
+		},
+		{
+			organization: {
+				name: "epitomiQ",
+				url: 'https://epitomiq.com',
+				logo: "organization-epitomiq.webp",
+				cover: 'organization-epitomiq-cover.webp'
+			},
+			jobs : [
+				{
+					name: "Fondateur & Président",
+					contract: "Gérant",
+					date: "Depuis 2018",
+					duration: "",
+					localisation: "Global",
+					description: `
+					Étude des algorithmes d'apprentissage automatique (Machine Learning)<br>
+					Développement d'un assistant virtuel adaptif<br>
+					Développement d'un moteur d'inférence long terme<br>
+					`
+			   }
+			]
+		},
+		{
+			organization: {
+				name: "abbeal",
+				url: 'https://www.abbeal.com',
+				logo: "organization-abbeal.webp",
+				cover: 'organization-abbeal-cover.webp'
+			},
+			jobs: [
+				{
+					name: "Consultant Devops",
+					contract: "Indépendant",
+					date: "Sep 2017 - Apr 2019",
+					duration: "1 an 7 mois",
+					localisation: "Paris 08, France",
+					description: `
+					Conseil sur les technologies Cloud<br>
+					Aide au recrutement d'ingénieurs DevOps
+					`
+				}
+			]
+		},
+		{
+			organization: {
+				name: "Backelite",
+				url: 'https://backelite.com',
+				logo: "organization-backelite.webp",
+				cover: 'organization-backelite-cover.webp'
+			},
+			jobs: [
+				{
+					name: "Software QA Consultant",
+					contract: "Indépendant",
+					date: "Aug 2017 - Sept 2017",
+					duration: "2 mois",
+					localisation: "Paris 08, France",
+					description: `Tests fonctionnels et exploratifs de 80+ applications dans les secteurs suivants :
+					<ul>
+						<li>Assurance</li>
+						<li>Bancaire</li>
+						<li>Commerce en ligne</li>
+					</ul>
+					En français, anglais. Sur ordinateur, Android et iOs.
+					`
+				}
+			]
+		},
+		{
+			organization: {
+				name: "StarDust",
+				url: 'https://stardust-testing.com',
+				logo: "organization-stardust.webp",
+				cover: 'organization-stardust-cover.webp'
+			},
+			jobs: [
+				{
+					name: "Consultant Test Logiciel",
+					contract: "Indépendant",
+					date: "Feb 2016 - Sep 2017",
+					duration: "1 an 7 mois",
+					localisation: "Marseille, France",
+					description: `Tests fonctionnels et exploratifs de 80+ applications dans les secteurs suivants :
+					<ul>
+						<li>Alimentation</li>
+						<li>Assurance</li>
+						<li>Bancaire</li>
+						<li>Commerce en ligne</li>
+						<li>Pari sportif</li>
+						<li>Télécommunications</li>
+						<li>Tourisme<br></li>
+					</ul>
+					En français, anglais et espagnol. Sur ordinateur, Android et iOs.
+					`
+				}
+			]
+		},
+		{
+			organization: {
+				name: "Naturex",
+				url: 'https://naturex.com',
+				logo: "organization-naturex.webp",
+				cover: 'organization-naturex-cover.webp'
+			},
+			jobs: [
+				{
+					name: "Developer",
+					contract: "CDD - Alternance",
+					date: "Sep 2013 - Sep 2015",
+					duration: "2 ans",
+					localisation: "Avignon, France",
+					description: `
+						Développement d'un outil de gestion de stock :
+						<ul>
+							<li>capable de garantir le suivi des articles</li>
+							<li>valoriser le stock</li>
+							<li>alerter les utilisateurs des mouvements réalisés (gestion du stock minimum, système d'approbation, Reporting)</li>
+						</ul>
+
+						Dans le cadre de la fiabilisation des systèmes existants, je dirige le projet de développement d'un outil de supervision applicative pour 300+ machines.
+					`
+				}
+			]
+		},
+		{
+			organization: {
+				name: "Lustucru Rivoire & Carret - Groupe PASTACORP",
+				url: 'https://www.lustucru.fr',
+				logo: "organization-lustucru.webp",
+				cover: 'organization-lustucru-cover.webp'
+			},
+			jobs: [
+				{
+					name: "Assistant IT Manager",
+					contract: "CDD - Alternance",
+					date: "Sep 2011 - Sep 201",
+					duration: "2 ans",
+					localisation: "Aix-en-Provence, France",
+					description: `
+						Chargé du support technique, je veille à garantir un service de qualité aux utilisateurs de l'entreprise.<br><br>
+
+						Ma fonction plurielle m'a permis d'acquérir de l'expérience dans la résolution de problèmes liés au matériel :
+						<ul>
+							<li>réparation d'ordinateurs,</li>
+							<li>intégration de systèmes (serveurs, poste clients, éléments de réseaux - routeurs, pare-feux, etc.).</li>
+						</ul>
+
+						Le besoin d'outil dans le cadre de l'amélioration des méthodes de travail m'amène à développer des outils :
+						<ul>
+							<li>gestionnaire de stocks,</li>
+							<li>gestionnaire de connexions multiples (RDP, VNC) sous C#.</li>
+						</ul>
+					`
+				}
+			]
+		}
+	]
+}
+
+career.experiences.forEach(experience => {
+	experience.jobs.forEach(job =>  {
+		job.description.replace(/^\t{3}/gm, '');
+	})
+});
+
+export default career;
--- src/routes/career/index.svelte
+++ src/routes/career/index.svelte
@@ -1,54 +1,233 @@
-<svelte:head>
-	<title>Parcours professionnel — Bruno Brise</title>
-	<meta name="description" content="Parcours professionnel de Bruno Brito Semedo">
-</svelte:head>
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`career.json`).then(r => r.json()).then(career => {
+			return { career };
+		});
+	}
+</script>
+
+<script>
+	export let career;
+</script>
 
 <style>
+	h2 {
+		font-weight: 600;
+	}
+
+	#career {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.title-cover {
+		background-color: var(--blue);
+		color: var(--white);
+		line-height: 2.5;
+		padding: 0 24px;
+		vertical-align: middle;
+		justify-items: center;
+	}
+
+	.title-cover-inner {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
 	.source {
 		font-size: 0.4em;
-		display: inline;
+		display: inline-block;
 		text-decoration: none;
 		margin-left: 0.5em;
 	}
 
 	.source-format {
-		float: right;
+		display: inline-block;
 		list-style: none;
-		margin: 0 1em;
+		margin-top: 8px;		
+		margin-left: 1.5em;
+	}
+
+	.experiences {
+		display:flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+
+	.experience {
+		display:flex;
+		flex-direction: column;
+		width:100%;
+		margin-bottom: 1em;
+		box-shadow: 0px 2px 3px var(--silver);
+	}
+
+	.experience:last-child .experience-job {
+		border-bottom: none;
+		border: none;
+	}
+
+	.experience-organization-cover {
+		position: relative;
+		height: 120px;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		background-size: cover;
+		background-position: center center;
+		background-repeat: no-repeat;
+	}
+
+	.organization-logo {
+		background-color: var(--white);
+		width: 48px;
+		position: absolute;
+		bottom: -24px;
+		right: 16px;
+		padding: 4px;
+		border: 1px solid var(--silver);
+	}
+
+	.experience-jobs {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		width: 100%;
+	}
+
+	.experience-job {
+		padding: 1em;
+	}
+
+	.experience-job > .name {
+		font-weight: 800;
+		font-size: 1.2em;
+	}
+
+	.experience-job > .date {
+		color: var(--gray);
+		font-size: 0.8em;
+	}
+
+	.experience-job > .date > .duration {
+		float: right;
+		background-color: var(--gray-dark);
+		color: var(--white);
+		padding: 0.1em 0.5em;
+		margin-top:-2px;
+		margin-left: 0.5em;
+		font-size: 1em;
+		cursor: default;
 		border-radius: 3px;
 	}
 
-	@media screen and (min-width: 56em) {
+	.experience-job > .description {
+		margin-top: 0.5em;
+		padding: 0.2em 0 0.2em 0.5em;
+		border-left: 2px solid var(--gray-light);
+	}
+
+	.experience-job > .description > :global(ul) {
+		list-style: none;
+		margin: 0.2em 0 1em;
+	}
+
+	.experience-job > .description :global(li) {
+		margin-left: 1.2em;
+	}
+
+	.experience-job > .description :global(li):before {
+		content: "✔ "
+	}
+
+	.experience-job > .organization > .contract:before {
+		content: "\2022  "
+	}
+
+	@media (min-width: 56em) {
+		.title-cover-inner {
+			max-width: 28em;
+			margin:0 auto;
+		}
 
 		.source-format {
 			float: none;
 			display: inline-block;
-			margin: 0 0.5em;
+			margin: 0 1em;
 		}
 
 		.source-format svg {
 			width: 24px;
 			height: 24px;
 		}
+
+		.organization-logo {
+			width: 72px;
+			bottom: -36px;
+			right: 28px;
+		}
+
+		.experience-organization-cover {
+			height: 340px;
+		}
 	}
 </style>
 
+<svelte:head>
+	<title>Parcours professionnel — Bruno Brise</title>
+	<meta name="description" content="Parcours professionnel de Bruno Brito Semedo">
+</svelte:head>
 
-<div id="career" class="container">
-	<h1>Parcours
-		<ul class="source">
-			<li class="source-format">
-				<a
-					href="https://www.linkedin.com/in/brunobritosemedo/"
-					target="_blank" rel="noopener">
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path></svg>
-				</a>
-			</li>
-			<li class="source-format">
-				<a href="brunobritosemedo.pdf">
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Adobe Acrobat Reader icon</title><path d="M23.598 15.368c-.71-.76-2.164-1.197-4.224-1.197-1.1 0-2.375.11-3.76.37-.782-.77-1.562-1.67-2.307-2.72-.53-.74-.993-1.52-1.42-2.29.813-2.54 1.206-4.61 1.206-6.1 0-1.672-.603-3.416-2.34-3.416-.533 0-1.066.325-1.35.8-.783 1.408-.43 4.493.917 7.54-.503 1.52-1.035 2.973-1.7 4.605-.578 1.376-1.244 2.794-1.923 4.096C2.793 18.64.267 20.49.03 21.94c-.104.547.074 1.05.457 1.45.133.11.636.545 1.48.545 2.59 0 5.32-4.28 6.707-6.86 1.065-.36 2.13-.687 3.193-1.015 1.168-.323 2.34-.583 3.405-.765 2.735 2.504 5.146 2.9 6.358 2.9 1.492 0 2.024-.617 2.203-1.122.28-.65.07-1.37-.252-1.74l.02.04zm-1.385 1.054c-.104.544-.638.906-1.386.906-.21 0-.39-.037-.603-.072-1.36-.325-2.633-1.016-3.903-2.106 1.25-.214 2.31-.25 2.98-.25.74 0 1.38.032 1.81.144.49.106 1.27.435 1.095 1.38h.02zm-7.523-1.707c-.92.19-1.914.414-2.944.693-.816.223-1.666.474-2.52.77.463-.902.854-1.774 1.208-2.603.428-1.02.78-2.07 1.135-3.046.35.61.74 1.23 1.13 1.78.64.87 1.31 1.7 1.98 2.42v-.02zM10.04 1.23c.145-.29.43-.436.678-.436.745 0 .887.868.887 1.56 0 1.168-.354 2.942-.96 4.967-1.062-2.82-1.135-5.18-.603-6.09zM6.138 18.127C4.328 21.17 2.59 23.06 1.525 23.06c-.21 0-.387-.075-.53-.183-.214-.216-.32-.472-.248-.76.213-1.09 2.236-2.613 5.392-3.99z"></path></svg>
-				</a>
-			</li>
-		</ul>
+<div id="career">
+	<h1 class="title-cover">
+		<div class="title-cover-inner">Parcours
+			<ul class="source">
+				<li class="source-format">
+					<a href="brunobritosemedo.pdf">
+						<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Adobe Acrobat Reader icon</title><path d="M23.598 15.368c-.71-.76-2.164-1.197-4.224-1.197-1.1 0-2.375.11-3.76.37-.782-.77-1.562-1.67-2.307-2.72-.53-.74-.993-1.52-1.42-2.29.813-2.54 1.206-4.61 1.206-6.1 0-1.672-.603-3.416-2.34-3.416-.533 0-1.066.325-1.35.8-.783 1.408-.43 4.493.917 7.54-.503 1.52-1.035 2.973-1.7 4.605-.578 1.376-1.244 2.794-1.923 4.096C2.793 18.64.267 20.49.03 21.94c-.104.547.074 1.05.457 1.45.133.11.636.545 1.48.545 2.59 0 5.32-4.28 6.707-6.86 1.065-.36 2.13-.687 3.193-1.015 1.168-.323 2.34-.583 3.405-.765 2.735 2.504 5.146 2.9 6.358 2.9 1.492 0 2.024-.617 2.203-1.122.28-.65.07-1.37-.252-1.74l.02.04zm-1.385 1.054c-.104.544-.638.906-1.386.906-.21 0-.39-.037-.603-.072-1.36-.325-2.633-1.016-3.903-2.106 1.25-.214 2.31-.25 2.98-.25.74 0 1.38.032 1.81.144.49.106 1.27.435 1.095 1.38h.02zm-7.523-1.707c-.92.19-1.914.414-2.944.693-.816.223-1.666.474-2.52.77.463-.902.854-1.774 1.208-2.603.428-1.02.78-2.07 1.135-3.046.35.61.74 1.23 1.13 1.78.64.87 1.31 1.7 1.98 2.42v-.02zM10.04 1.23c.145-.29.43-.436.678-.436.745 0 .887.868.887 1.56 0 1.168-.354 2.942-.96 4.967-1.062-2.82-1.135-5.18-.603-6.09zM6.138 18.127C4.328 21.17 2.59 23.06 1.525 23.06c-.21 0-.387-.075-.53-.183-.214-.216-.32-.472-.248-.76.213-1.09 2.236-2.613 5.392-3.99z"></path></svg>
+					</a>
+				</li>
+				<li class="source-format">
+					<a
+						href="https://www.linkedin.com/in/brunobritosemedo/"
+						target="_blank" rel="noopener">
+						<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path></svg>
+					</a>
+				</li>
+			</ul>
+		</div>
 	</h1>
+
+	<div class="container">
+		<div class="resume">
+			<section id="experiences">
+				<h2>Expérience</h2>
+				<div class="experiences">
+					{#each career.experiences as experience}
+					<div class="experience">
+						<div class="experience-organization-cover" style="background-image: url({experience.organization.cover})">
+							<a href="{experience.organization.url}">
+								<img class="organization-logo" src="{experience.organization.logo}" alt="{experience.organization.name} logo">
+							</a>
+						</div>
+						<div class="experience-jobs">
+							{#each experience.jobs as job}
+							<div class="experience-job">
+								<p class="name">{job.name}</p>
+								<p class="organization">{experience.organization.name} <span class="contract">{job.contract}</span></p>
+								<p class="date">{job.date}{#if job.duration != ""} <span class="duration">{job.duration}</span>{/if}</p>
+								{#if job.description != ""}
+								<p class="description description-wrap">{@html job.description}</p>
+								{/if}
+							</div>
+							{/each}
+						</div>
+					</div>
+					{/each}
+				</div>
+			</section>
+		</div>
+	</div>
 </div>
