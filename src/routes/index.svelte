30090512d5f9aa37bfa3e27e6de6c8380f332a16
Bruno Brito Semedo <ego@brunobrise.com>
2020-03-20 09:19:35 +0100

    update home page style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,127 +1,100 @@
 <style>
-	#avatar {
-		height: 110vh;
-	}
-
-	.avatar-image {
-		width: 100%;
-		height: 100%;
-		background: url(cover.webp) no-repeat center center;
-		-webkit-background-size: cover;
-		-moz-background-size: cover;
-		-o-background-size: cover;
-		background-size: cover;
-		background-size: cover;
-		position:relative;
-	}
-
-	#bio {
-		text-align:left;
-	}
-
-	.pun {
-		color: var(--gray);
-	}
-
-	section .container {
-		padding-top: 1em;
-		padding-bottom: 1em;
-	}
-
-	.section-cover {
+	.container {
 		display: flex;
-		align-items: center;
+		flex-direction: column;
 	}
 
-	.section-cover .button{
-		margin-top: 2em;
+	.fullname {
+		font-size: 1.3em;
 	}
 
-	.section-title {
+	.role {
+		/* text-align: right; */
+		font-size: 2em;
+	}
+
+	.introduction {
+		font-size: 1.2em;
 		text-align: center;
+		padding-left: 25%;
+		text-align: left;
+	}
+		
+	.container > .picture-wrapper {
+		padding: 1em 0;
 	}
 
-	.section-columns {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: stretch;
-		margin: 0 auto;
+	.container > .picture-wrapper > .picture {
+		object-fit: cover;
+		box-sizing: border-box;
+		width:100%;
+		height:100%;
 	}
 
-	.section-columns:first-child {
-		border: none;
+	.bio {
+		font-size: 1em;
+		text-align: left;
+		text-align: justify;
 	}
 
-	.section-column {
-		padding: 0 24px;
-		word-break: break-word;
-	}
+	@media screen and (min-width: 72em) {
+		.container {
+			overflow: hidden;
+			height: 100vh;
+			display: grid;
 
-	.section-column p {
-		margin-top:1em;
-	}
+			grid-template-columns: [cfirst] repeat(5, [cleft] 8vw) [cmiddle] 4em repeat(5, [cright] 8vw) [clast];
+			grid-template-rows: [rtop] repeat(15, [rstep] 6vh) auto [rbottom];
 
-	.section-column:first-child {
-		margin-top:0;
-	}
-
-	.quote {
-		margin: 0 auto;
-		padding: 1em;
-	}
-
-	.quote a {
-		display: flex;
-		align-items: center;
-		text-decoration: none;
-	}
-
-	.quote-author-image {
-		width: 72px;
-		border-radius: 50%;
-		float: left;
-		padding-right: 1em;
-	}
-
-	.quote-content {
-		font-style: italic;
-	}
-
-	.quote-author {
-		font-weight: bold;
-	}
-
-	#contact {
-		background-color: var(--blue);
-		color: var(--white);
-		min-height: 50vh;
-		text-align: center;
-	}
-
-	@media screen and (min-width:56em) {
-		.section-title {
-			padding: 0 24px;
+			align-items: stretch;
 		}
 
-		.section-columns {
-			flex-wrap: nowrap;
-			padding:1.5em 0;
+		.container > .picture-wrapper {
+			overflow: hidden;
+			padding: 0;
+			grid-area: rtop / cright 1 / rbottom / clast;
+		}
+		
+		.container > .picture-wrapper > .picture {
+			object-fit: cover;
+			box-sizing: border-box;
+			width:100%;
+			height:100%;
 		}
 
-		.section-column {
-			padding: 1em;
-			max-width: 47%;
-			border-right: 1px solid var(--white-whisper);
+		.fullname {
+			font-size: 1.3em;
+			grid-column-start: cleft 1;
+			grid-column-end: cleft 3;
+			grid-row-start: rstep 3;
+			grid-row-end: rstep 4;
+
+			align-self: center;
 		}
 
-		.section-column:last-child {
-			border: none;
+		.role {
+			font-size: 2.2em;
+			grid-column-start: cleft 3;
+			grid-column-end: cmiddle;
+			grid-row-start: rstep 3;
+			grid-row-end: rstep 4;
+
+			align-self: center;
 		}
 
-		.quote {
-			max-width:50vw;
+		.introduction {
+			font-size: 2em;
+			padding-left: 0;
+			grid-column-start: cleft;
+			grid-column-end: cmiddle;
+			grid-row-start: rstep 4;
+			grid-row-end: rstep 5;
+		}
+
+		.bio {
+			grid-column-start: cleft 2;
+			grid-column-end: cmiddle;
+			grid-row-start: rstep 7;
 		}
 	}
 </style>
@@ -129,37 +102,32 @@
 <svelte:head>
 </svelte:head>
 
-<section id="avatar">
-	<div class="avatar-image"></div>
-</section>
-
-<section id="bio">
-	<div class="section-inner container">
-		<h2 class="section-title">Quelques mots à mon sujet</h2>
-		<div class="section-columns">
-			<div class="section-column">
-				<p>Bruno Bri<span class="pun">to</span> Se<span class="pun">medo</span> est un ingénieur Cloud œuvrant à l'accélération de la performance technologique des entreprises via les pratiques DevOps.</p>
-				<p>Il travaille actuellement avec <a href="http://wynd.eu" target="_blank" rel="noopener">Wynd, éditeur de logiciel développant une plateforme de commerce unifié</a> qui a sécurisé un investissment total de 112M€ en 2020. Startup au sein de laquelle il participe à l'extension de la plateforme ainsi qu'au maintien de la haute disponibilité.</p>
-				<p>Bruno propose également de mettre à votre service ses 12 années d'expériences professionnelles. L'architecture et la maintenance de votre infrastructure sur les plateformes Cloud (AWS, GCP, Azure) à l'aide d'outils d'Infrastructure as Code. Il propose aussi le développement logiciel sous forme d'API, interfaces web et applications mobiles en respectant les <a href="https://agilemanifesto.org/" target="_blank" rel="noopener">méthodes Agile</a>, <a href="https://12factor.net/" target="_blank" rel="noopener">Twelve-Factor</a> ou encore <a href="https://blog.cleancoder.com/" target="_blank" rel="noopener">Clean Code</a> (TDD).</p>
-			</div>
-			<div class="section-column">
-				<p>Entrepreneur dynamique, Bruno développe des produits répondant à des problématiques aussi diverses et variées que celles liées à l'accessibilité, l'isolement social, l'image de marque personnelle, la mise en relation, l'émancipation de soi. Il adhère fortement à l'idée que la technologie est un facteur de développement humain. L'automatisation des tâches chronophages ainsi qu'à faible valeur cognitive est son leitmotiv.</p>
-				<p>Dans son rôle d'intervenant au sein d'associations telles que 100 000 entrepreneurs, il prône l'accompagnement des plus jeunes dans leurs actions entrepreneuriales. Cette activité créative associée à une recherche méthodique d'informations est essentielle à l'élaboration de cadres de travail productifs.</p>
-				<p>Bruno Brise est la manifestation du désir d'exploration continue de son créateur.</p>
-			</div>
-		</div>
-		<div class="quote">
-			<a href="https://www.hanselman.com/" target="_blank" rel="noopener">
-				<img class="quote-author-image" src="people-scott-hanselman.webp" alt="Photo de Scott Hanselman">
-				<p class="quote-content">«&nbsp;L'automatisation est le plus puissant des outils dont nous disposons en tant que développeurs.&nbsp;»&nbsp;—&nbsp;<span class="quote-author">Scott Hanselman</span></p>
-			</a>
-		</div>
+<div class="container">
+	<div class="fullname">
+		<p>Bruno Brito Semedo</p>
 	</div>
-</section>
+	<div class="role">
+		<p>Ingénieur Cloud</p>
+	</div>
+	<div class="introduction">
+		<p>Accélère la performance technologique des entreprises par les pratiques DevOps.</p>
+	</div>
+	<div class="picture-wrapper">
+		<img class="picture" src="cover.webp" alt="Bruno Brito Semedo's picture"/>
+	</div>
+	<div id="bio-1" class="bio">
+		<p>Bruno travaille actuellement avec <a href="http://wynd.eu" target="_blank" rel="noopener">Wynd, éditeur de logiciel développant une plateforme de commerce unifié</a> qui a sécurisé un investissment total de 112M€ en 2020. Startup au sein de laquelle il participe à l'extension de la plateforme ainsi qu'au maintien de la haute disponibilité.</p>
+		<br><p>Il propose également de mettre à votre service ses 12 années d'expériences professionnelles. L'architecture et la maintenance de votre infrastructure sur les plateformes Cloud (AWS, GCP, Azure) à l'aide d'outils d'Infrastructure as Code. Il propose aussi le développement logiciel sous forme d'API, interfaces web et applications mobiles en respectant les <a href="https://agilemanifesto.org/" target="_blank" rel="noopener">méthodes Agile</a>, <a href="https://12factor.net/" target="_blank" rel="noopener">Twelve-Factor</a> ou encore <a href="https://blog.cleancoder.com/" target="_blank" rel="noopener">Clean Code</a> (TDD).</p>
+		<br><p>Entrepreneur dynamique, Bruno développe des produits répondant à des problématiques aussi diverses et variées que celles liées à l'accessibilité, l'isolement social, l'image de marque personnelle, la mise en relation, l'émancipation de soi. Il adhère fortement à l'idée que la technologie est un facteur de développement humain. L'automatisation des tâches chronophages à faible valeur cognitive est son leitmotiv.</p>
+		<!-- <p>Dans son rôle d'intervenant au sein d'associations telles que 100 000 entrepreneurs, il prône l'accompagnement des plus jeunes dans leurs actions entrepreneuriales. Cette activité créative associée à une recherche méthodique d'informations est essentielle à l'élaboration de cadres de travail productifs.</p> -->
+		<!-- <p>Bruno Brise est la manifestation du désir d'exploration continue de son créateur.</p> -->
+	</div>
+</div>
 
-<section id="contact" class="section-cover">
+
+<!-- <section id="contact" class="section-cover">
 	<div class="container section-inner">
 		<h2 class="section-title">Pour une infrastructure élastique.<br>Supervision, haute disponibilité et scalabilité.</h2>
 		<a class="button" href="mailto:bonjour@brunobrise.com?subject=Prise%20de%20contact&body=Bonjour Bruno,">ME CONTACTER</a>
 	</div>
-</section>
+</!-->
